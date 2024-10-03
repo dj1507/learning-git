f74b2d6ee4eba6a303e61466e255c596cbb001fc
Dhanesh <jdhanesh3@gmail.com>
2024-10-03 19:13:30 +0530

    Created add two number function

--- First_program.java
+++ First_program.java
@@ -1,7 +1,12 @@
 public class First_program {
+
+    static void add(int a, int b){
+        System.out.println("Add two numbers "+ (a+b));
+    }
+
     public static void main(String[] args) {
-        System.out.println("Testing");    
+        System.out.println("Testing");
+        add(10,20);    
     }
 
 }
-
