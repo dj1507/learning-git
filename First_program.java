7f6eae53df7862a133b7c3ca3d7679e477fe242e
Dhanesh <jdhanesh3@gmail.com>
2024-10-05 17:12:47 +0530

    Exploring extends keyword

--- First_program.java
+++ First_program.java
@@ -1,12 +1,21 @@
-public class First_program {
+class Arithmetic{
+    
+    public void multiple(int a, int b){
+        System.out.println("Multiplication of two numbers "+ (a*b));
+    }
+}
+
+class First_program extends Arithmetic{
 
     static void add(int a, int b){
         System.out.println("Add two numbers "+ (a+b));
     }
-
+    
     public static void main(String[] args) {
         System.out.println("Testing");
-        add(10,20);    
-    }
+        add(10,20);  
+        First_program obj = new First_program();
+        obj.multiple(10,20);
+    }   
 
 }
